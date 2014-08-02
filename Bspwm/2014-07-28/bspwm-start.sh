e0a55e89964bdb880fbeb96120591ce8b80bf99a
marcoscan <marcoscan.com@gmail.com>
2014-08-02 18:06:08 +0200

    Update

--- Bspwm/2014-07-28/bspwm-start.sh
+++ Bspwm/2014-07-28/bspwm-start.sh
@@ -9,12 +9,6 @@ sh ~/.fehbg &
 (sleep 6 && setxkbmap -layout it)
 (sleep 1 && conky -c /home/marco/.config/bspwm/conkyrc-todo)
 
-#(sleep 1 && test -p /tmp/win.fifo || mkfifo /tmp/win.fifo) &
-#(sleep 3 && sh ~/.config/bspwm/Scripts/bspwm-wins.sh) &
-#(sleep 2 && bspc control --subscribe > /tmp/win.fifo) &
-
-#(sleep 6 && cat /tmp/win.fifo  | awk -F /Ltiled/ '{ exit(system("if [ $(bspc query -W -d | wc -l) -le 1 ] ; then bspc desktop --layout monocle ; exit 1; fi ")); }') &
-
 sxhkd &
 
 exec bspwm
